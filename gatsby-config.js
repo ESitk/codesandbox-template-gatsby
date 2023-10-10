cc4679f28549b4a30cd731328696e90bacc3d6d4
EvanSit <104138392+ESitk@users.noreply.github.com>
2023-10-10 01:19:51 +0000

    Update 2 files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
       title: `Gatsby Project`,
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://www.studentFirst.online`,
     },
     plugins: [
 
